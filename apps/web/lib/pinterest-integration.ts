b973731941287fde1c5352d0188ace939c548640
lionnesswalk-prog <lionnesswalk@gmail.com>
2026-09-15 18:54:57 +0530

    Use shared Postgres runtime mode for social provider storage

--- apps/web/lib/pinterest-integration.ts
+++ apps/web/lib/pinterest-integration.ts
@@ -1,6 +1,7 @@
 import { getPrisma } from "./prisma";
 import { getSession } from "./auth";
 import { canEncryptIntegrations, decryptIntegrationSecret, encryptIntegrationSecret } from "./integration-crypto";
+import { isPostgresBackend } from "./runtime-mode";
 
 type PinterestTokenResponse = {
   access_token: string;
@@ -35,7 +36,7 @@ export type PinterestConnection = {
 };
 
 function usePostgres() {
-  return process.env.DATA_BACKEND === "postgres";
+  return isPostgresBackend();
 }
 
 function storageReady() {
